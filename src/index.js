d0995edad4e2fdc88d7e44b1f954f0e54ac4cfcd
Dev.Umar <umarraja230@gmail.com>
2023-09-02 23:29:03 +0500

    Modify & Fix Scroll on page change

--- src/index.js
+++ src/index.js
@@ -6,14 +6,18 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./Store/store";
+import ScrollToTop from "./Hooks/ScrollToTop";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
+      <>
+        <ScrollToTop />
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </>
     </BrowserRouter>
   </React.StrictMode>
 );
