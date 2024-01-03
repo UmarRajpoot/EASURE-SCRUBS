4f2edf4469d35ccf4c18fa740306c4901aaa184b
Dev.Umar <umarraja230@gmail.com>
2024-01-03 16:13:13 +0500

    All Filtered added

--- src/pages/ProductList/ProductList.js
+++ src/pages/ProductList/ProductList.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link, useParams, useNavigate } from "react-router-dom";
 import AccordionComp from "../../components/AccordionComp/AccordionComp";
 import { Box, Divider, Image, Stack, Text } from "@chakra-ui/react";
 import axios from "axios";
@@ -8,15 +8,44 @@ import { CheckIcon } from "@chakra-ui/icons";
 
 const ProductList = () => {
   const params = useParams();
+  const Navigate = useNavigate();
 
   const FiltersList = [
-    { name: "Scrub Top", mensTo: "", womenTo: "" },
-    { name: "Scrub Pants", mensTo: "", womenTo: "" },
-    { name: "Fit Guide", mensTo: "", womenTo: "" },
-    { name: "Color", mensTo: "", womenTo: "" },
+    { name: "Scrub Top" },
+    { name: "Scrub Pants" },
+    { name: "Fit Guide" },
+    // { name: "Color" },
   ];
 
+  const Colors = [
+    {
+      name: "BLACK",
+      code: "#000000",
+    },
+    {
+      name: "CEIL-BLUE",
+      code: "#92A1CF",
+    },
+    {
+      name: "WINE",
+      code: "#722F37",
+    },
+    { name: "ROYAL-BLUE", code: "#4169E1" },
+    { name: "PEWT", code: "#899499" },
+    { name: "NAVY-BLUE", code: "#000080" },
+  ];
+
+  const [getIndProducts, setIndProducts] = useState([]);
   const [AllProducts, setAllProducts] = useState([]);
+
+  const [selected, setSelected] = useState("");
+
+  const [colorSelect, setColorSelect] = useState("");
+
+  useEffect(() => {
+    // console.log("Added");
+    setAllProducts(getIndProducts);
+  }, [getIndProducts]);
 
   const WomenList = [
     "3982c326-3160-463a-a8b4-8d82865e23d3",
@@ -42,6 +71,36 @@ const ProductList = () => {
     "d79a4676-0ae1-46d8-845d-ba4eda5f474a",
     "e3be9847-9ff8-4091-9e7c-2ddd19fd13b0",
     "8bbba27e-70ae-48ea-b3ac-40c3e5b6e4f7",
+    "3e99340b-4d47-4f1c-a9ec-85efea5230b3",
+  ];
+
+  const ScrubWomenTop = [
+    "3982c326-3160-463a-a8b4-8d82865e23d3",
+    "544a84dc-ec52-4b02-b4e7-8568e4aa8574",
+    "36d8a10a-3ffe-4c4e-8aa8-4242dfcc3de2",
+    "50e81ca6-2a68-4e47-bca2-6822bbf9eb9d",
+    "604a4d77-99c2-4387-b3a1-25ac809f279c",
+    "6e002472-8941-448c-b664-9708ae7d8165",
+    "c8552fa8-1163-475b-8a61-d60ee247826f",
+    "81287ebc-616c-4784-a784-67811e405f4d",
+    "b89fa01c-5c93-44bd-91f8-89cac605f248",
+    "4bb35bc4-ea4c-4e1a-8fb7-2007c3a646e7",
+    "d79a4676-0ae1-46d8-845d-ba4eda5f474a",
+    "8bbba27e-70ae-48ea-b3ac-40c3e5b6e4f7",
+  ];
+
+  const ScrubWomenPants = [
+    "e413e985-3977-463c-a83f-33cd584e9e2c",
+    "ea88584d-50b8-4732-aa66-c1d016d4b7d6",
+    "0d8bb012-74f9-4cde-894d-a5a477bb9321",
+    "6dde2c76-352b-4683-b954-f0a10ba73928",
+    "31717dc4-b7ab-42cb-91f5-0385db6175b3",
+    "3f9a826f-6694-482d-a96a-f37044198c26",
+    "f637416c-ec0b-486d-aa9c-e22c25048a61",
+    "701f0a28-2c93-43cf-9212-d55a0ad593a9",
+    "449f50da-68c7-4168-a705-4d1e22d8bab8",
+    "6e5710d3-0f41-4ef5-8e29-89cc7075cd59",
+    "e3be9847-9ff8-4091-9e7c-2ddd19fd13b0",
     "3e99340b-4d47-4f1c-a9ec-85efea5230b3",
   ];
 
@@ -116,7 +175,7 @@ const ProductList = () => {
               return 0;
             });
           });
-          return setAllProducts(temp_data);
+          return setIndProducts(temp_data);
         } else {
           let temp_data = [];
           MenList.map((list_id) => {
@@ -127,7 +186,7 @@ const ProductList = () => {
               return 0;
             });
           });
-          return setAllProducts(temp_data);
+          return setIndProducts(temp_data);
         }
       })
       .catch((error) => {
@@ -162,7 +221,9 @@ const ProductList = () => {
               src={mensT.productimage && mensT.productimage[0]}
               // onLoad={() => console.log("loading")}
             /> */}
-            {mensT.productimage && onHover === mensT.id ? (
+            {mensT.productimage &&
+            colorSelect === "" &&
+            onHover === mensT.id ? (
               <Image
                 src={mensT.productimage[1]}
                 // onLoad={() => console.log("loading")}
@@ -247,7 +308,78 @@ const ProductList = () => {
       </Link>
     );
   };
-  const [selected, setSelected] = useState("");
+
+  const ToggleFilter = (data) => {
+    switch (data.name) {
+      case "Scrub Top":
+        if (params.category === "men") {
+          let temp_scrubTop = [];
+          console.log("Men Search");
+          ScrubMenTop.map((list_id) => {
+            return getIndProducts.filter((scrubtopMen_prod) => {
+              if (scrubtopMen_prod.id === list_id) {
+                return temp_scrubTop.push(scrubtopMen_prod);
+              }
+              return 0;
+            });
+          });
+          setAllProducts(temp_scrubTop);
+        } else if (params.category === "women") {
+          let temp_scrubTop = [];
+          ScrubWomenTop.map((list_id) => {
+            return getIndProducts.filter((scrubtopWoemn_prod) => {
+              if (scrubtopWoemn_prod.id === list_id) {
+                return temp_scrubTop.push(scrubtopWoemn_prod);
+              }
+              return 0;
+            });
+          });
+          return setAllProducts(temp_scrubTop);
+        }
+        break;
+      case "Scrub Pants":
+        if (params.category === "men") {
+          let temp_scrubPant = [];
+          ScrubMenPents.map((list_id) => {
+            return getIndProducts.filter((scrubpantMen_prod) => {
+              if (scrubpantMen_prod.id === list_id) {
+                return temp_scrubPant.push(scrubpantMen_prod);
+              }
+              return 0;
+            });
+          });
+          return setAllProducts(temp_scrubPant);
+        } else if (params.category === "women") {
+          let temp_scrubPant = [];
+          ScrubWomenPants.map((list_id) => {
+            return getIndProducts.filter((scrubpantWomen_prod) => {
+              if (scrubpantWomen_prod.id === list_id) {
+                return temp_scrubPant.push(scrubpantWomen_prod);
+              }
+              return 0;
+            });
+          });
+          return setAllProducts(temp_scrubPant);
+        }
+        break;
+
+      case "Fit Guide":
+        if (params.category === "men") {
+          return Navigate("/fitguide/men");
+        } else if (params.category === "women") {
+          return Navigate("/fitguide/women");
+        }
+        break;
+
+      default:
+        console.log("work default");
+        return setAllProducts(getIndProducts);
+    }
+  };
+
+  // useEffect(() => {
+  //   console.log("Effected");
+  // }, [selected]);
 
   return (
     <Stack minH={"100vh"} direction={"row"}>
@@ -267,35 +399,10 @@ const ProductList = () => {
                     onClick={() => {
                       if (selected === index) {
                         setSelected("");
-                        getProducts();
-                      } else {
+                        setAllProducts(getIndProducts);
+                      } else if (selected !== index) {
                         setSelected(index);
-                        switch (data.name) {
-                          case "Scrub Top":
-                            let temp_scrubTop = [];
-                            ScrubMenTop.map((list_id) => {
-                              return AllProducts.filter((scrubtopMen_prod) => {
-                                if (scrubtopMen_prod.id === list_id) {
-                                  return temp_scrubTop.push(scrubtopMen_prod);
-                                }
-                                return 0;
-                              });
-                            });
-                            return setAllProducts(temp_scrubTop);
-                          case "Scrub Pants":
-                            let temp_scrubPant = [];
-                            ScrubMenPents.map((list_id) => {
-                              return AllProducts.filter((scrubpantMen_prod) => {
-                                if (scrubpantMen_prod.id === list_id) {
-                                  return temp_scrubPant.push(scrubpantMen_prod);
-                                }
-                                return 0;
-                              });
-                            });
-                            return setAllProducts(temp_scrubPant);
-                          default:
-                            return getProducts();
-                        }
+                        ToggleFilter(data);
                       }
                     }}
                   >
@@ -306,6 +413,55 @@ const ProductList = () => {
               </>
             );
           })}
+          <Box
+            display={"flex"}
+            alignItems={"center"}
+            justifyContent={"space-between"}
+          >
+            <Text userSelect={"none"} fontSize={"md"}>
+              Colors
+            </Text>
+            {colorSelect !== "" && (
+              <Text
+                cursor={"pointer"}
+                onClick={() => {
+                  setColorSelect("");
+                  setAllProducts(getIndProducts);
+                }}
+              >
+                Reset
+              </Text>
+            )}
+          </Box>
+          <Box flexWrap={"wrap"} display={"flex"}>
+            {Colors.map((color, index) => {
+              return (
+                <Box
+                  key={index}
+                  w={"5"}
+                  h={"5"}
+                  bgColor={color.code}
+                  rounded={"full"}
+                  m={"1"}
+                  title={color.name}
+                  cursor={"pointer"}
+                  ring={colorSelect === index ? "2" : "0"}
+                  onClick={() => {
+                    setColorSelect(index);
+                    let allpro = getIndProducts.filter((prod_col) => {
+                      if (
+                        prod_col.colors?.name.toUpperCase() ===
+                        color.name.toUpperCase()
+                      ) {
+                        return prod_col;
+                      }
+                    });
+                    return setAllProducts(allpro);
+                  }}
+                ></Box>
+              );
+            })}
+          </Box>
         </Stack>
       </Box>
       <Box
